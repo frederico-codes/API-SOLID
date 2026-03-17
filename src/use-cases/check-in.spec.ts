cf599ebf6fa533d3f6676219218282f25ae8c7a2
frederico nakajima <fredericonakajimanakajima@gmail.com>
2026-03-17 13:32:38 -0300

    TDD & Mocking, Validando data do check-in

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,23 +1,82 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInUseCase } from "./check-in";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { Decimal } from "@prisma/client/runtime/library";
 
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe("Check-in Use Case", () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInUseCase(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository);
+    vi.useFakeTimers();
+
+    gymsRepository.items.push({
+      id: "gym-01",
+      title: "JavaScript Gym",
+      description: "",
+      phone: "",
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
+  });   
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to check in", async () => {  
     const { checkIn } = await sut.execute({
-    gymId: "gym-01",
-    userId: "user-01",
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
     });
     expect(checkIn.id).toEqual(expect.any(String));
   });
   
+
+  it("should not be able to check in twice in the same day", async () => {  
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0)); // 20 de janeiro de 2022 às 8:00:00
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+  
+    await expect(() => sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    })).rejects.toBeInstanceOf(Error);    
+  });
+  
+  it("should be able to check in twice in the same day but in different days", async () => { 
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0)); // 20 de janeiro de 2022 às 8:00:00 
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0)); // 21 de janeiro de 2022 às 8:00:00
+  
+    const { checkIn } = await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+      userLatitude: 0,
+      userLongitude: 0,
+    })  
+
+    expect(checkIn.id).toEqual(expect.any(String)); 
+  });
 });
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,9 +1,13 @@
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { GymsRepository } from "@/repositories/gyms-repository";
 
 interface CheckInUseCaseRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLongitude: number;
 }
 
 interface CheckInUseCaseResponse {
@@ -11,19 +15,35 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase{
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
-  async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+  async execute({ 
+    userId, 
+    gymId 
+  }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    
+  const gym = await this.gymsRepository.findById(gymId);
+
+    if (!gym) {
+      throw new ResourceNotFoundError();
+    } 
+
+    //Calcular a distância entre o usuário e a academia
+
+    const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(userId, new Date());
+
+    if (checkInOnSameDay) {
+      throw new Error();
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId,
     });
-
-  
-    
-    return {
-      checkIn,
-    };
+    return { checkIn };
   }
 }
 
